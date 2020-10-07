84026da1d797e9d0fa2a617e56c337c62d8b8545
Aminur <aminur6264@gmail.com>
2020-10-08 00:25:57 +0600

    no message

--- ServerSidejQueryDatatableDotNetCore3.1/ServerSidejQueryDatatableDotNetCore3.1/Controllers/ProductController.cs
+++ ServerSidejQueryDatatableDotNetCore3.1/ServerSidejQueryDatatableDotNetCore3.1/Controllers/ProductController.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ServerSidejQueryDatatableDotNetCore3._1.Models;
 
 namespace ServerSidejQueryDatatableDotNetCore3._1.Controllers
 {
     public class ProductController : Controller
     {
+        DataTableDbContext db;
+        public ProductController()
+        {
+            db = new DataTableDbContext(new DbContextOptions<DataTableDbContext>());
+        }
         public IActionResult Index()
         {
             return View();
@@ -15,6 +22,18 @@ namespace ServerSidejQueryDatatableDotNetCore3._1.Controllers
         public IActionResult NewProduct()
         {
             return View();
+        }
+
+        public IActionResult AllProduct()
+        {
+            var result = db.Products.Select((x, index) => new 
+            {
+                SL = index + 1,
+                Id = x.Id,
+                Name = x.Name
+            }).ToList();
+
+            return Json(new { data = result });
         }
     }
 }
