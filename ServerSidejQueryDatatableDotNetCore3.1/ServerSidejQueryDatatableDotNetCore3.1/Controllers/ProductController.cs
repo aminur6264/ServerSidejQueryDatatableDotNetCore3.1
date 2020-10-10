74081e4add2ebe72f52adcf72c9e9daaf7da1862
Aminur <aminur6264@gmail.com>
2020-10-10 20:59:25 +0600

    Product List With Entry Page Complete

--- ServerSidejQueryDatatableDotNetCore3.1/ServerSidejQueryDatatableDotNetCore3.1/Controllers/ProductController.cs
+++ ServerSidejQueryDatatableDotNetCore3.1/ServerSidejQueryDatatableDotNetCore3.1/Controllers/ProductController.cs
@@ -20,9 +20,38 @@ namespace ServerSidejQueryDatatableDotNetCore3._1.Controllers
         {
             return View();
         }
-        public IActionResult NewProduct()
+        public IActionResult NewProduct(int? id)
         {
-            return View();
+            Product product = new Product();
+            if (id != null)
+                product = db.Products.FirstOrDefault(x => x.Id == id);
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult NewProduct(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                //Product product = new Product();
+                if (product.Id != 0)
+                {
+                    var existProduct = db.Products.FirstOrDefault(x => x.Id == product.Id);
+                    existProduct.Name = product.Name;
+                    existProduct.Brand = product.Brand;
+                    existProduct.Model = product.Model;
+                    existProduct.Size = product.Size;
+                    existProduct.Price = product.Price;
+                    db.Products.Update(existProduct);
+                }
+                else
+                    db.Products.Add(product);
+
+                db.SaveChanges();
+                return RedirectToAction("Index","Product");
+            }
+            else
+                return View(product);
         }
 
         [HttpPost]
@@ -37,57 +66,77 @@ namespace ServerSidejQueryDatatableDotNetCore3._1.Controllers
             string productPrice = dtParameters.Columns[5].Search.Value.ToString();
 
 
-            #region Order By
-            var orderCriteria = string.Empty;
-            string orderByDir = "";
-            if (dtParameters.Order != null)
-            {
-                switch (dtParameters.Order[0].Column)
-                {
-                    case 1:
-                        orderCriteria = "BND.Id";
-                        break;
-                    case 2:
-                        orderCriteria = "BND.Name";
-                        break;
-                    case 3:
-                        orderCriteria = "CTG.Id";
-                        break;
-                    case 4:
-                        orderCriteria = "CTG.Name";
-                        break;
-                    case 5:
-                        orderCriteria = "TYP.Id";
-                        break;
-                    case 6:
-                        orderCriteria = "TYP.Name";
-                        break;
-                    default:
-                        orderCriteria = "Id";
-                        break;
-                }
 
-                orderByDir = dtParameters.Order[0].Dir.ToString().ToLower();
-            }
-            else
-            {
-                orderCriteria = "CTG.Id";
-            }
-            string orderBy = " ORDER BY " + orderCriteria + " " + orderByDir;
-            #endregion
 
             List<Product> products = db.Products.ToList();
+            int totalResultsCount = products.Count;
             if (productName != "")
                 products = products.Where(x => x.Name.ToLower().Contains(productName.ToLower())).ToList();
-            if(productBrand != "")
+            if (productBrand != "")
                 products = products.Where(x => x.Brand.ToLower().Contains(productBrand.ToLower())).ToList();
             if (productModel != "")
                 products = products.Where(x => x.Model.ToLower().Contains(productModel.ToLower())).ToList();
             if (productSize != "")
                 products = products.Where(x => x.Size.ToLower().Contains(productSize.ToLower())).ToList();
+            if (productPrice != "")
+                products = products.Where(x => x.Price.ToString().ToLower().Contains(productPrice.ToLower())).ToList();
 
 
-            var result = products.Select((x, index) => new
+            #region Order By
+            var orderCriteria = string.Empty;
+            string orderByDir = dtParameters.Order[0].Dir.ToString().ToLower();
+            if (dtParameters.Order != null)
+            {
+                switch (dtParameters.Order[0].Column)
+                {
+                    case 1:
+                        if (orderByDir.ToLower() == "asc")
+                            products = products.OrderBy(x => x.Name).ToList();
+                        else
+                            products = products.OrderByDescending(x => x.Name).ToList();
+                        break;
+                    case 2:
+                        if (orderByDir.ToLower() == "asc")
+                            products = products.OrderBy(x => x.Brand).ToList();
+                        else
+                            products = products.OrderByDescending(x => x.Brand).ToList();
+                        break;
+                    case 3:
+                        if (orderByDir.ToLower() == "asc")
+                            products = products.OrderBy(x => x.Model).ToList();
+                        else
+                            products = products.OrderByDescending(x => x.Model).ToList();
+                        break;
+                    case 4:
+                        if (orderByDir.ToLower() == "asc")
+                            products = products.OrderBy(x => x.Size).ToList();
+                        else
+                            products = products.OrderByDescending(x => x.Size).ToList();
+                        break;
+                    case 5:
+                        if (orderByDir.ToLower() == "asc")
+                            products = products.OrderBy(x => x.Price).ToList();
+                        else
+                            products = products.OrderByDescending(x => x.Price).ToList();
+                        break;
+                        //case 6:
+                        //    if (orderByDir.ToLower() == "asc")
+                        //        products = products.OrderBy(x => x.Id).ToList();
+                        //    else
+                        //        products = products.OrderByDescending(x => x.Id).ToList();
+                        //    break;
+                        //default:
+                        //    if (orderByDir.ToLower() == "asc")
+                        //        products = products.OrderBy(x => x.Id).ToList();
+                        //    else
+                        //        products = products.OrderByDescending(x => x.Id).ToList();
+                        //    break;
+                }
+            }
+            #endregion
+
+            int filteredResultsCount = products.Count;
+            var result = products.Skip(dtParameters.Start).Take(dtParameters.Length).Select((x, index) => new
             {
                 SL = index + 1,
                 Id = x.Id,
@@ -98,7 +147,13 @@ namespace ServerSidejQueryDatatableDotNetCore3._1.Controllers
                 Price = x.Price
             }).ToList();
 
-            return Json(new { data = result });
+            return Json(new
+            {
+                draw = dtParameters.Draw,
+                recordsTotal = totalResultsCount,
+                recordsFiltered = filteredResultsCount,
+                data = result
+            });
         }
     }
 }
--- ServerSidejQueryDatatableDotNetCore3.1/ServerSidejQueryDatatableDotNetCore3.1/Models/Product.cs
+++ ServerSidejQueryDatatableDotNetCore3.1/ServerSidejQueryDatatableDotNetCore3.1/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,6 +13,7 @@ namespace ServerSidejQueryDatatableDotNetCore3._1.Models
         public string Brand { get; set; }
         public string Model { get; set; }
         public string Size { get; set; }
+        [Required]
         public decimal Price { get; set; }
     }
     public class Invoice
